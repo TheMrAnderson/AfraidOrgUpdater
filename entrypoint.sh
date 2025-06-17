bc28f5798fe623ad3cd93b6690329d83730f2057
Matt Anderson <mpanderson06@fastmail.com>
2025-06-17 15:47:07 -0500

    Updating entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-crontab -l | { cat; echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"; } | crontab -
-crontab -l | { cat; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1\n"; } | crontab -
+(crontab -l 2>/dev/null; echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1") | crontab -
 
 crond -f -l 0
