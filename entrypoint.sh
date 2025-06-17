ccef6f9ff183a13237e76b0f2f9d5e1f1b847a51
Matt Anderson <mpanderson06@fastmail.com>
2025-06-17 15:54:29 -0500

    Fixing another error

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+# Ensure /root/.cache exists for crontab
+mkdir -p /root/.cache
+
 (crontab -l 2>/dev/null; echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin") | crontab -
 (crontab -l 2>/dev/null; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1") | crontab -
 
-crond -f -l 0
+crond -f -d 0
