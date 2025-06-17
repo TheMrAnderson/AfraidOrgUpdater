c8ef84e20bcf2d5fc1f61326c516fbc8f585719f
Matt Anderson <mpanderson06@fastmail.com>
2025-06-17 08:01:54 -0500

    Moving logging to terminal log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 crontab -l | { cat; echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"; } | crontab -
-crontab -l | { cat; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /tmp/freedns_domain.log 2>/dev/null"; } | crontab -
+crontab -l | { cat; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1\n"; } | crontab -
 
 crond -f -l 0
