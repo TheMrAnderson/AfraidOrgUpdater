f91643a8da15cf8283a91f849fffcb2ee1bece09
Matt Anderson <mpanderson06@fastmail.com>
2025-06-17 18:46:55 -0500

    Adding silent flag again

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 mkdir -p /root/.cache
 
 (crontab -l 2>/dev/null; echo "PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin") | crontab -
-(crontab -l 2>/dev/null; echo "*/5 * * * * sleep 9 ; curl http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "*/5 * * * * sleep 9 ; curl -s http://sync.afraid.org/u/$1/ >> /proc/1/fd/1 2>&1") | crontab -
 
 # Start crond in foreground
 crond -f
